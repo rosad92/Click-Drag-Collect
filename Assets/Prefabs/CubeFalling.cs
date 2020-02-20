1ff319cfffda66426de816632553009a7249a74c
rosad92 <49042220+rosad92@users.noreply.github.com>
2020-02-20 14:17:13 -0500

    Destroy Cubes

---
+++ Assets/Prefabs/CubeFalling.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeFalling : MonoBehaviour{
+
+    void OnTriggerEnter(Collider other) {
+        if(this.tag == other.tag) {
+            Destroy(this.gameObject);
+        }
+    }
+
+    void Update() {
+       if(this.transform.position.y <(-10)) {
+            Destroy(this.gameObject);
+        }
+    }
+}
