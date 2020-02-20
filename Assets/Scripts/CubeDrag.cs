6401f60fdec24f2550a0965d7eaa0d873bd8748e
rosad92 <49042220+rosad92@users.noreply.github.com>
2020-02-20 12:59:01 -0500

    Click and Drag cubes

---
+++ Assets/Scripts/CubeDrag.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeDrag : MonoBehaviour{
+
+    public bool drag = false;
+    // Update is called once per frame
+    void Update(){
+        if (drag) {
+            Vector3 mousePos2D = Input.mousePosition;
+            mousePos2D.z = -Camera.main.transform.position.z;
+            Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
+            Vector3 pos = this.transform.position;
+            pos.x = mousePos3D.x;
+            pos.y = mousePos3D.y;
+            this.transform.position = pos;
+            if (Input.GetMouseButtonUp(0)) {
+                drag = false;
+            }
+        }   
+
+    }
+
+    private void OnMouseDown() {
+        drag = true;
+    }
+}
