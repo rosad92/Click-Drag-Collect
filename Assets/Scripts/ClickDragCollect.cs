54f2353abc5b60075c470058aae0a8dc619baef1
rosad92 <49042220+rosad92@users.noreply.github.com>
2020-02-20 11:31:16 -0500

    Drop Several, No Random

--- Assets/Scripts/ClickDragCollect.cs
+++ Assets/Scripts/ClickDragCollect.cs
@@ -4,9 +4,42 @@ using UnityEngine;
 
 public class ClickDragCollect : MonoBehaviour{
     [Header("Set in Inspector")]
-    public GameObject cubePrefab;
+    public GameObject rCubePrefab;
+    public GameObject bCubePrefab;
+    public GameObject yCubePrefab;
+    public GameObject pCubePrefab;
+    public float cubeDist;
+    public int cubeNum;
+    public Vector3 cubeCenter;
     void Awake() {
-        GameObject cube = Instantiate<GameObject>(cubePrefab);
-        cube.transform.position = new Vector3(0, 6, 0);
+        Invoke("dropCubes", 2f);
+    }
+
+    void dropCubes() {
+        GameObject rCube = Instantiate<GameObject>(rCubePrefab);
+        rCube.transform.position = new Vector3(1, 6, 0);
+        GameObject bCube = Instantiate<GameObject>(bCubePrefab);
+        bCube.transform.position = new Vector3(-1, 6, 0);
+        GameObject yCube = Instantiate<GameObject>(yCubePrefab);
+        yCube.transform.position = new Vector3(3, 6, 0);
+        GameObject pCube = Instantiate<GameObject>(pCubePrefab);
+        pCube.transform.position = new Vector3(-3, 6, 0);
+
+
+        /* for (int i = 0; i<cubeNum; i++) {
+
+
+            GameObject cube = Instantiate<GameObject>(rCubePrefab);
+            Vector3 cubePos = Vector3.zero;
+            cubePos.y = 6;
+            cubePos.x = ((i+1) / 2) * cubeDist;
+            if(i%2 == 1) {
+                cubePos.x = -cubePos.x;
+            }
+            cube.transform.position = cubePos;
+        }*/
+
+
+
     }
 }
