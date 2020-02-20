c9d459add44d2d1defdcd5632a5b80112974df3b
rosad92 <49042220+rosad92@users.noreply.github.com>
2020-02-20 12:29:01 -0500

    Adjust positions and create other prefabs

--- Assets/Scripts/ClickDragCollect.cs
+++ Assets/Scripts/ClickDragCollect.cs
@@ -11,39 +11,37 @@ public class ClickDragCollect : MonoBehaviour{
     public GameObject placementPrefab;
     public float cubeDist;
     public int cubeNum;
-    public Vector3 cubeCenter;
     void Awake() {
+        createPlacement();
         Invoke("dropCubes", 2f);
+    }
+
+    void createPlacement() {
         for (int i = 0; i < cubeNum; i++) {
             GameObject cube = Instantiate<GameObject>(placementPrefab);
             Vector3 cubePos = Vector3.zero;
             cubePos.y = -6;
             cubePos.z = 0.5f;
-            cubePos.x = ((i + 1) / 2) * cubeDist;
-            if (i % 2 == 1) {
-                cubePos.x = -cubePos.x;
-            }
+            cubePos.x = (i * cubeDist);
             cube.transform.position = cubePos;
         }
+
     }
     void dropCubes() {
+        //Add all cube colors to list
         List<GameObject> cubes = new List<GameObject>();
         cubes.Add(rCubePrefab);
         cubes.Add(bCubePrefab);
         cubes.Add(yCubePrefab);
         cubes.Add(pCubePrefab);
 
-
-
+        // place them to drop in a random order
         for (int i = 0; i< cubeNum; i++) {
             int randCube = Random.Range(0, cubes.Count);
             GameObject cube = Instantiate<GameObject>(cubes[randCube]);
             Vector3 cubePos = Vector3.zero;
             cubePos.y = 6;
-            cubePos.x = ((i+1) / 2) * cubeDist;
-            if(i%2 == 1) {
-                cubePos.x = -cubePos.x;
-            }
+            cubePos.x = i * cubeDist;
             cube.transform.position = cubePos;
             cubes.RemoveAt(randCube);
         }
