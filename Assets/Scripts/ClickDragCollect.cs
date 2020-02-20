d3aff38e57367a29464fd8378f6ec15d1c64ebec
rosad92 <49042220+rosad92@users.noreply.github.com>
2020-02-20 11:00:20 -0500

    Drop Cube

---
+++ Assets/Scripts/ClickDragCollect.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickDragCollect : MonoBehaviour{
+    [Header("Set in Inspector")]
+    public GameObject cubePrefab;
+    void Awake() {
+        GameObject cube = Instantiate<GameObject>(cubePrefab);
+        cube.transform.position = new Vector3(0, 6, 0);
+    }
+}
